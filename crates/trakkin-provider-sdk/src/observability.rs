3779b4a249741c5f6453a4d94b73cc78e673f7ee
Elias Benbourenane <eliasbenbourenane@gmail.com>
2026-09-16 01:38:21 +0000

    refactor(provider): simplify logging implementation
    Removed multi-format support and and simplified the tracing usage.

--- crates/trakkin-provider-sdk/src/observability.rs
+++ crates/trakkin-provider-sdk/src/observability.rs
@@ -1,44 +1,24 @@
 use std::{env, error::Error, io};
+use tracing_subscriber::filter::LevelFilter;
 
-use tracing_subscriber::{
-    EnvFilter, fmt::format::FmtSpan, layer::SubscriberExt, util::SubscriberInitExt,
-};
-
-pub const PROVIDER_LOG_FILTER_ENV: &str = "TRAKKIN_PROVIDER_LOG";
-pub const PROVIDER_LOG_FORMAT_ENV: &str = "TRAKKIN_PROVIDER_LOG_FORMAT";
+const LOG_LEVEL_ENV: &str = "TRAKKIN_LOG";
+const DEFAULT_LOG_LEVEL: &str = "info";
 
 pub fn init_provider_tracing(
     provider_id: &'static str,
-) -> Result<(), Box<dyn Error + Send + Sync>> {
-    let filter = env::var(PROVIDER_LOG_FILTER_ENV).unwrap_or_else(|_| "info".to_owned());
-    let filter = EnvFilter::try_new(filter)?;
-    let format = env::var(PROVIDER_LOG_FORMAT_ENV).unwrap_or_else(|_| "json".to_owned());
-    if format.eq_ignore_ascii_case("pretty") {
-        tracing_subscriber::registry()
-            .with(filter)
-            .with(
-                tracing_subscriber::fmt::layer()
-                    .with_ansi(false)
-                    .with_span_events(FmtSpan::NEW | FmtSpan::CLOSE)
-                    .with_writer(io::stderr),
-            )
-            .try_init()?;
-    } else {
-        tracing_subscriber::registry()
-            .with(filter)
-            .with(
-                tracing_subscriber::fmt::layer()
-                    .json()
-                    .flatten_event(true)
-                    .with_ansi(false)
-                    .with_span_events(FmtSpan::NEW | FmtSpan::CLOSE)
-                    .with_writer(io::stderr),
-            )
-            .try_init()?;
-    }
-    tracing::info!(
-        event = "provider.logging.initialized",
-        provider.id = provider_id
-    );
-    Ok(())
+) -> Result<tracing::Span, Box<dyn Error + Send + Sync>> {
+    let level = env::var(LOG_LEVEL_ENV).unwrap_or_else(|_| DEFAULT_LOG_LEVEL.to_owned());
+    tracing_subscriber::fmt()
+        .json()
+        .flatten_event(true)
+        .with_current_span(true)
+        .with_span_list(true)
+        .with_max_level(level.parse::<LevelFilter>()?)
+        .with_writer(io::stderr)
+        .try_init()?;
+    Ok(tracing::info_span!(
+        "provider.process",
+        provider.id = provider_id,
+        process.instance_id = tracing::field::Empty,
+    ))
 }
